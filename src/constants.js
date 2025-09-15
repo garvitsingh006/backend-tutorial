673cead589711853216fa3da17ab4e8a48b0abe4
Garvit Singh <garvitsingh006@gmail.com>
2025-09-15 19:20:05 +0530

    utils code discussion

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const DB_NAME = "backend_tut";
+export const PORT = process.env.PORT || 8000;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import dotenv from "dotenv";
+import { app } from "./app.js"
+import { PORT } from "./constants.js"
+
 dotenv.config({
     path: "./.env"
 });
-import connectDB from "./db/dbConnect.js";
 
-connectDB();
+
+import connectDB from "./db/dbConnect.js";
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed!!!", err)
+})
